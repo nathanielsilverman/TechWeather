1515ee2673a3f70a20440cc1787696a5d86717c0
Nathaniel Silverman <nathanielmsilverman@gmail.com>
2021-10-06 13:54:05 -0400

    route slash added

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require( "express" );
 
+
 const PORT = 6969;
 const app = express();
+
+app.get( "/", ( req, res ) =>
+{
+    res.send( "Good Day" );
+} );
 
 app.listen( PORT, () =>
 {
